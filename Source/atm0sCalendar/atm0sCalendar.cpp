017243ae2097dc0f3b865d251b73b5bf8493fb43
Shalini Roy <spacetimedestroyed@gmail.com>
2023-10-21 00:50:40 -0700

    first commit

---
+++ Source/atm0sCalendar/atm0sCalendar.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "atm0sCalendar.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, atm0sCalendar, "atm0sCalendar" );
